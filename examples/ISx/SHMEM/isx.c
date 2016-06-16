cc922bcf3cb31a13cdc6e7b6baf6d3ae92d98f7f
Vivek Kumar <vivek@titan-ext1.ccs.ornl.gov>
2016-06-15 23:50:53 -0400

    minor fix

--- examples/ISx/SHMEM/isx.c
+++ examples/ISx/SHMEM/isx.c
@@ -106,6 +106,7 @@ int main(const int argc,  char ** argv)
     avg_time *= 1000;
     avg_time_all2all *= 1000;
 
+    printf("\n============================ MMTk Statistics Totals ============================\n");
     if(NUM_ITERATIONS == 1) { //TODO: fix time calculation below for more number of iterations
       printf("time.mu\tt.ATA_KEYS\tt.MAKE_INPUT\tt.COUNT_BUCKET_SIZES\tt.BUCKETIZE\tt.COMPUTE_OFFSETS\tt.LOCAL_SORT\tnWorkers\tnPEs\n");
       double TIMES[TIMER_NTIMERS];
@@ -129,11 +130,6 @@ int main(const int argc,  char ** argv)
       printf("%.3f\t%.3f\t1\t%d\n",avg_time,avg_time_all2all,NUM_PES);
       printf("Total time: %.3f\n",avg_time);
     }
-
-
-    printf("\n============================ MMTk Statistics Totals ============================\n");
-    printf("time.mu\ttimeAll2All\tnWorkers\tnPEs\n");
-    printf("%.3f\t%.3f\t1\t%d\n",avg_time,avg_time_all2all,NUM_PES);
     printf("Total time: %.3f\n",avg_time);
     printf("------------------------------ End MMTk Statistics -----------------------------\n");
     printf("===== TEST PASSED in %.3f msec =====\n",(tTime*1000));
